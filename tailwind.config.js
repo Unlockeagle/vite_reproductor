48528943f169d1def09433d0e8f4c05aa753a504
Unlockeagle <unlock.eagle@gmail.com>
2023-06-24 22:02:05 -0400

    taildwind

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}"
+],
+  theme: {
+    extend: {
+      colors: {
+        'darkGreen': '#1DB956',
+        'green': '#1FD662',
+        'white': '#FEFEFE',
+        'black': '#191313'
+      },
+
+      fontFamily: {
+        mainFonts: ['Montserrat', 'sans-serif'],
+      }
+
+      
+
+
+
+
+    },
+  },
+  plugins: [],
+}
+
